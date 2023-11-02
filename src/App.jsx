911ad97c0b30347bc9ce601624a1982208c3651f
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-11-02 10:57:48 +0800

    with auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@ import logo from "/logo.png";
 import "./App.css";
 import FruitForm from "./Components/FruitForm";
 import FruitList from "./Components/FruitList";
+import { useState, useEffect } from "react";
+import AuthForm from "./Components/AuthForm";
+import { auth } from "./firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        setIsLoggedIn(true);
+        setUser(user);
+      }
+    });
+  }, []);
+
   return (
     <>
       <div>
@@ -11,9 +28,23 @@ function App() {
       </div>
       <h1>Fruit </h1>
       <div className="card">
-        <FruitForm />
+        {isLoggedIn ? <h2>Welcome back {user.email}</h2> : null}
+        {isLoggedIn ? (
+          <button
+            onClick={() => {
+              setIsLoggedIn(false);
+              signOut(auth);
+              setUser({});
+            }}
+          >
+            Logout!
+          </button>
+        ) : null}
+
+        {isLoggedIn ? <FruitForm /> : <AuthForm />}
+
+        <FruitList />
       </div>
-      <FruitList />
     </>
   );
 }
