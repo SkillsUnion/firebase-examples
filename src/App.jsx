aed7bdde089eae6c5753eccc82e9c451da71331b
terencegaffudSU <terence.gaffud@skillsunion.com>
2024-10-17 11:08:04 +0800

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from "/logo.png";
 import "./App.css";
 import FruitForm from "./Components/FruitForm";
 import FruitList from "./Components/FruitList";
@@ -6,9 +5,6 @@ import FruitList from "./Components/FruitList";
 function App() {
   return (
     <>
-      <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
-      </div>
       <h1>Fruit </h1>
       <div className="card">
         <FruitForm />
